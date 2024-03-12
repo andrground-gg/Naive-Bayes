adaeff277edb4df7b6152cc0bcb6f2febf2a99f9
andrground <av.andreevv@gmail.com>
2024-03-12 19:34:15 +0100

    Test gaussian naive bayes from sklearn lib

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -2,7 +2,9 @@ import numpy as np
 
 class NaiveBayes:
     def __init__(self):
-        pass
+        self.__classes = None
+        self.__class_priors = None
+        self.__feature_likelihoods = None
 
     def fit(self, X, y):
         pass
