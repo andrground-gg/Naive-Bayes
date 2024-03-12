a12c05e37458694fcf7fb809f6fcaba8b822ca9a
andrground <av.andreevv@gmail.com>
2024-03-12 18:36:26 +0100

    Set up functions

---
+++ metrics.py
@@ -0,0 +1,11 @@
+def accuracy(y_true, y_pred):
+    pass
+
+def precision(y_true, y_pred):
+    pass
+
+def recall(y_true, y_pred):
+    pass
+
+def f1(y_true, y_pred):
+    pass
---
+++ src/naive_bayes.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class NaiveBayes:
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
