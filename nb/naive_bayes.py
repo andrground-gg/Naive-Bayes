767673a18f99c856a7fed51515eb1209920a0c2c
andrground <av.andreevv@gmail.com>
2024-03-13 16:06:31 +0100

    Make it work for numerical data

--- nb/naive_bayes.py
+++ nb/naive_bayes.py
@@ -1,14 +1,12 @@
-from math import exp
-from math import pi
-from math import sqrt
 import numpy as np
 
 class NaiveBayes:
-    def __init__(self):
+    def __init__(self, log=False):
         self.__classes = None
         self.__class_priors = None
         self.__means = {}
         self.__variances = {}
+        self.__log = log
 
     def fit(self, X, y):
         self.__classes = np.unique(y)
@@ -18,22 +16,33 @@ class NaiveBayes:
             self.__means[c] = X_c.mean(axis=0)
             self.__variances[c] = X_c.var(axis=0)
 
-    def predict(self, X):
-        pass
+        if (self.__log):
+            print('Classes: ', self.__classes)
+            print('Class priors: ', self.__class_priors)
+            print('Means: ', self.__means)
+            print('Variances: ', self.__variances)
 
-    # def __mean(self, a):
-    #     return sum(a) / len(a)
-    
-    # def __variance(self, a):
-    #     mean = self.__mean(a)
-    #     return sum([(x - mean) ** 2 for x in a]) / len(a)
+    def predict(self, X):
+        probabilities = self.__calculate_class_probabilities(X)
+        predictions = probabilities[1] > probabilities[0]
+        return predictions.astype('int')
     
     def __calculate_likelihood(self, x, mean, variance):
-        exponent = exp(-(x - mean) ** 2 / (2 * variance))
-        return (1 / (sqrt(2 * pi * variance))) * exponent
+        exponent = np.exp(-(x - mean) ** 2 / (2 * variance))
+        return (1 / (np.sqrt(2 * np.pi * variance))) * exponent
     
     def __calculate_class_probabilities(self, x):
-        pass
+        probabilities = {}
+        for c in self.__classes:
+            likelihood = np.prod(self.__calculate_likelihood(x, self.__means[c], self.__variances[c]), axis=1)
+            probabilities[c] = self.__class_priors[c] * likelihood
+
+        if (self.__log):
+            print('Class probabilities: ', probabilities)
+
+        return probabilities
+        
+
     
     
 
